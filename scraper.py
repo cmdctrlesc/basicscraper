773fd29d08912785d51b9a40ee551175def9024c
cmdctrlesc <alanxyz210@gmail.com>
2023-06-21 17:04:31 +0200

    write urls to file

--- scraper.py
+++ scraper.py
@@ -1,31 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 
+logo_urls = []
+
 url = 'https://webflow.com'
+
+#so podvojeni?
 
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text, features='html.parser')
 divs = soup.find_all("div", {"class": "intro-logos_wrapper"})
+
 for div in divs:
     for element in div.find_all("img"):
         pic_url = element['src']
-        # print(pic_url)
+        if pic_url not in logo_urls:
+        #avoid adding duplicates
+
+            logo_urls.append(pic_url) 
 
 
 #second page        
 
-# url2 = 'https://scale.com'
+url2 = 'https://scale.com'
 
-# page2 = requests.get(url2)
+page2 = requests.get(url2)
 
-# soup2 = BeautifulSoup(page2.text, features='html.parser')
+soup2 = BeautifulSoup(page2.text, features='html.parser')
 
-# lis = soup2.find_all("li", {"class": "flex justify-center items-center"})
-# for li in lis:
-#     for element in li.find_all("img"):
-#         pic_url = element.get("src")
-#         print(url2 + pic_url)
+lis = soup2.find_all("li", {"class": "flex justify-center items-center"})
+for li in lis:
+    for element in li.find_all("img"):
+        pic_url = element.get("src")
+        logo_urls.append(url2 + pic_url)
 
 # with open('test.svg', 'w') as file:
 #     data = requests.get('https://scale.com/_next/image?url=%2Fstatic%2Fimages%2Flogos%2Fcustomers%2Fetsy.svg&w=96&q=100').text
@@ -38,14 +46,17 @@ url3 = 'https://www.deel.com'
 page3 = requests.get(url3)
 
 soup3 = BeautifulSoup(page3.text, features='html.parser')
-# print(soup3)
-
-# divs3 = soup3.find_all("div", {'class': ['single-image', 'slick-slide', 'slick-cloned', 'logo-scroll-looper']})
 
 divs3 = soup3.find_all("div", {'class': ['scroll-looper-sec',  'slick-initialized', 'slick-slider']})
-# print(divs3)
+
 for div in divs3:
     for element in div.find_all("img"):
         pic_url = element['src']
-        print(pic_url)
+        logo_urls.append(pic_url)
 
+multiline_string = ''.join([url + '\n' for url in logo_urls])
+#create a multiline string from the list of image urls
+
+text_file = open('image_urls.txt', "w")
+text_file.write(multiline_string)
+text_file.close()
