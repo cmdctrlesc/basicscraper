d45c667bea3e10adc9bced75c219af14600d2ebe
cmdctrlesc <alanxyz210@gmail.com>
2023-06-21 20:19:02 +0200

    add brand names to txt file

--- scraper.py
+++ scraper.py
@@ -3,60 +3,84 @@ from bs4 import BeautifulSoup
 
 logo_urls = []
 
-url = 'https://webflow.com'
+# scrape first website
 
-#so podvojeni?
+url1 = "https://webflow.com"
 
-page = requests.get(url)
+page1 = requests.get(url1)
 
-soup = BeautifulSoup(page.text, features='html.parser')
-divs = soup.find_all("div", {"class": "intro-logos_wrapper"})
+soup1 = BeautifulSoup(page1.text, features="html.parser")
+divs1 = soup1.find_all("div", {"class": "intro-logos_wrapper"})
 
-for div in divs:
+for div in divs1:
     for element in div.find_all("img"):
-        pic_url = element['src']
-        if pic_url not in logo_urls:
-        #avoid adding duplicates
+        pic_url = element["src"]
+        brand_name = element["alt"].replace("logo", "")
+        brand_dict = {brand_name.upper(): pic_url}
 
-            logo_urls.append(pic_url) 
+        if brand_dict not in logo_urls:  # avoid adding duplicates
+            logo_urls.append(brand_dict)
 
+# scrape second website
 
-#second page        
-
-url2 = 'https://scale.com'
+url2 = "https://scale.com"
 
 page2 = requests.get(url2)
 
-soup2 = BeautifulSoup(page2.text, features='html.parser')
+soup2 = BeautifulSoup(page2.text, features="html.parser")
 
 lis = soup2.find_all("li", {"class": "flex justify-center items-center"})
 for li in lis:
     for element in li.find_all("img"):
-        pic_url = element.get("src")
-        logo_urls.append(url2 + pic_url)
+        pic_url = element["src"]
+        brand_name = element["alt"].replace("logo", "").replace("-", " ")
+        brand_dict = {brand_name.upper(): url2 + pic_url}
+        logo_urls.append(brand_dict)
 
-# with open('test.svg', 'w') as file:
-#     data = requests.get('https://scale.com/_next/image?url=%2Fstatic%2Fimages%2Flogos%2Fcustomers%2Fetsy.svg&w=96&q=100').text
-#     file.write(data)
+# scrape third website
 
-#third page
-
-url3 = 'https://www.deel.com'
+url3 = "https://www.deel.com"
 
 page3 = requests.get(url3)
 
-soup3 = BeautifulSoup(page3.text, features='html.parser')
+soup3 = BeautifulSoup(page3.text, features="html.parser")
 
-divs3 = soup3.find_all("div", {'class': ['scroll-looper-sec',  'slick-initialized', 'slick-slider']})
+divs3 = soup3.find_all(
+    "div", {"class": ["scroll-looper-sec", "slick-initialized", "slick-slider"]}
+)
 
 for div in divs3:
     for element in div.find_all("img"):
-        pic_url = element['src']
-        logo_urls.append(pic_url)
+        pic_url = element["src"]
+        brand_name = element["alt"]
+        cleaned_name = (
+            brand_name.lower().replace("-", " ").replace("_", " ").replace(".", " ")
+        )
+        extra_strings = [
+            "white",
+            "logo",
+            "1",
+            "2019",
+            "2016",
+            " ",
+        ]  # clean brand name by removing unnecessary strings
+        final_brand_name = " ".join(
+            [x for x in cleaned_name.split() if x not in extra_strings]
+        )
+        brand_dict = {final_brand_name.upper(): pic_url}
 
-multiline_string = ''.join([url + '\n' for url in logo_urls])
-#create a multiline string from the list of image urls
+        logo_urls.append(brand_dict)
 
-text_file = open('image_urls.txt', "w")
+# write image urls to file
+
+multiline_string = "".join(
+    [
+        name + ": " + url + "\n"
+        for logo_url in logo_urls
+        for name, url in logo_url.items()
+    ]
+)  # create a multiline string from dict of image
+
+text_file = open("image_urls.txt", "w")
 text_file.write(multiline_string)
 text_file.close()
